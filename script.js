d2f7260da6f143cb21285e241c28d374d93ec945
arunt2005 <arunt2005@gmail.com>
2026-09-21 20:59:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,166 @@
+
+
+let originalQuestions = [
+    { q: "Which disease is also called breakbone fever because of the extreme pain it causes in the joints?", options: ["Dengue", "Polio", "Influenza", "Typhoid"], answer: "Dengue" }, 
+    { q: "Also known as varicella, which viral infection causes itchy blisters all over the body?", options: ["Measles", "Mumps", "Smallpox", "Chickenpox"], answer: "Chickenpox" }, 
+    { q: "Which disease is also called lockjaw?", options: ["Tetanus", "Tuberculosis", "Plaque", "Hepatitis"], answer: "Tetanus" }, 
+    { q: "Which disease is caused by the bite of the female anopheles mosquito?", options: ["Cholera", "Plague", "Malaria", "Dengue"], answer: "Malaria" }, 
+    { q: "Which of these is a serious infection of the lungs in which the air sacs fill with fluid and pus leading to severe breathing problems?", options: ["Jaundice", "Pneumonia", "Typhoid", "Cholera"], answer: "Pneumonia" }, 
+    { q: "Which viral infection of the respiratory passage is spread by breathing airborne droplets infected with one of the four viruses—A, B, C or D?", options: ["Polio", "Influenza", "Measles", "Typhoid"], answer: "Influenza" }, 
+    { q: "Which of these diseases, called consumption in the 18th century, leads to the formation of hard nodules that break down the respiratory tissues and form cavities in the lungs?", options: ["Cholera", "Plague", "Dengue", "Tuberculosis"], answer: "Tuberculosis" }, 
+    { q: "Which bacterial disease is caused by Salmonella typhi?", options: ["Dengue", "Malaria", "Typhoid", "Ebola"], answer: "Typhoid" },
+	{
+    "q": "Which term is used for the system of music generally associated with southern India?",
+    "options": [
+      "Sangam",
+      "Carnatic",
+      "Sugam"
+    ],
+    "answer": "Carnatic"
+  },
+  {
+    "q": "Which of these titles is given to a guru or learned musician?",
+    "options": [
+      "Pandit/Ustad",
+      "Mahatma/Shayar",
+      "Kalakar/Zafar"
+    ],
+    "answer": "Pandit/Ustad"
+  },
+  {
+    "q": "Which of these is a devotional composition?",
+    "options": [
+      "Tala",
+      "Bhajan",
+      "Vistaar"
+    ],
+    "answer": "Bhajan"
+  },
+  {
+    "q": "In music schools, a shishya is a...",
+    "options": [
+      "Teacher",
+      "Student",
+      "Singer"
+    ],
+    "answer": "Student"
+  },
+  {
+    "q": "Collectively, the notes sa re ga ma pa dha ni are called...",
+    "options": [
+      "Pukar",
+      "Jhalak",
+      "Sargam"
+    ],
+    "answer": "Sargam"
+  },
+  {
+    "q": "The first line of a song is called...",
+    "options": [
+      "Mukhda",
+      "Raga",
+      "Antara"
+    ],
+    "answer": "Mukhda"
+  },
+  {
+    "q": "Which of these is a musical structure of more than four notes with an identity and mood?",
+    "options": [
+      "Raga",
+      "Sarangi",
+      "Baithak"
+    ],
+    "answer": "Raga"
+  },
+  {
+    "q": "The system of music generally associated with northern India is called...",
+    "options": [
+      "Hindustani",
+      "Uttaranchal",
+      "Swadesh"
+    ],
+    "answer": "Hindustani"
+  },
+  {
+    question: 'In which sport must a player perform a series of actions: a hop, a step and a jump?',
+    options: ['Triple jump', 'Pole vault', 'High jump'],
+    answer: 'Triple jump'
+  },
+  {
+    question: 'What do participants throw in javelin throw?',
+    options: ['Spear', 'Ball', 'Disc'],
+    answer: 'Spear'
+  },
+  {
+    question: 'How many events does the heptathlon consist of?',
+    options: ['Five', 'Six', 'Seven'],
+    answer: 'Seven'
+  },
+  {
+    question: 'What are short distance races, covering distances of 100 m, 200 m and 400 m, called?',
+    options: ['Push', 'Sprint', 'Dart'],
+    answer: 'Sprint'
+  },
+  {
+    question: 'With which of these events would you associate a distance of 42.2 km?',
+    options: ['Steeplechase', 'Marathon', 'Long Jump'],
+    answer: 'Marathon'
+  },
+  {
+    question: 'Which of these is a long distance race in which runners jump over hurdles and water ditches?',
+    options: ['Pole vault', 'Triathlon', 'Steeplechase'],
+    answer: 'Steeplechase'
+  },
+  {
+    question: 'In the Olympics, men compete in the decathlon and women compete in the...',
+    options: ['Hexathlon', 'Heptathlon', 'Nonathlon'],
+    answer: 'Heptathlon'
+  },
+  {
+    question: 'Generally, how many runners are there in each team in a relay race?',
+    options: ['One', 'Two', 'Four'],
+    answer: 'Four'
+  },
+  {
+    "question": "Which mammal, covered with horny scales, usually defends itself by curling into a ball?",
+    "options": ["Aardvark", "Meerkat", "Pangolin"],
+    "answer": "Pangolin"
+  },
+  {
+    "question": "Which is the largest Asian antelope?",
+    "options": ["Sambar", "Hangul", "Nilgai"],
+    "answer": "Nilgai"
+  },
+  {
+    "question": "Which black-and-white mammal is known for its foul-smelling spray?",
+    "options": ["Gerenuk", "Skunk", "Beaver"],
+    "answer": "Skunk"
+  },
+  {
+    "question": "What kind of creatures are black widows?",
+    "options": ["Spiders", "Lizards", "Scorpions"],
+    "answer": "Spiders"
+  },
+  {
+    "question": "Which is the largest marsupial in the world?",
+    "options": ["Parma wallaby", "Common Wombat", "Red kangaroo"],
+    "answer": "Red kangaroo"
+  },
+  {
+    "question": "Which bird’s pink colour comes from its food which is rich in plant pigments called carotenoids?",
+    "options": ["Flamingo", "Sarus crane", "Kingfisher"],
+    "answer": "Flamingo"
+  },
+  {
+    "question": "Which is the heaviest lizard on Earth?",
+    "options": ["Gila monster", "Komodo dragon", "Chameleon"],
+    "answer": "Komodo dragon"
+  },
+  {
+    "question": "A species of which of these creatures is considered the most intelligent of all invertebrates?",
+    "options": ["Earthworm", "Jellyfish", "Octopus"],
+    "answer": "Octopus"
+  }
+  
+];
+
